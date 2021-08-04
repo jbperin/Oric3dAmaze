d5b3d1ed284a99a55c740760feeee2140b8742bc
Jean-Baptiste PERIN <jean-baptiste.perin@orange.com>
2021-08-04 16:10:04 +0200

    update preprocessor to match forthcoming OSDK 1.19

--- src/osdk_config.bat
+++ src/osdk_config.bat
@@ -12,7 +12,7 @@ REM charset_s image_s
 SET OSDKCOMP=-O2
 
 
-REM need to add %OSDKPREPROC% in parameters of cpp.exe preprocess command in file %OSDK%\bin\make.bat
+REM need to add %OSDKCPPFLAGS% in parameters of cpp.exe preprocess command in file %OSDK%\bin\make.bat
 REM 0:english 1:french 2:spanish 3:italian
 IF "%LANG%"=="EN" GOTO English
 IF "%LANG%"=="FR" GOTO French
@@ -20,19 +20,19 @@ IF "%LANG%"=="ES" GOTO Spanish
 IF "%LANG%"=="IT" GOTO Italian
 
 :English
-SET OSDKPREPROC=-DLANG=4
+SET OSDKCPPFLAGS=-DLANG=4
 SET OSDKNAME=POG_OP1_EN
 GOTO End
 :French
-SET OSDKPREPROC=-DLANG=1
+SET OSDKCPPFLAGS=-DLANG=1
 SET OSDKNAME=POG_OP1_FR
 GOTO End
 :Spanish
-SET OSDKPREPROC=-DLANG=2
+SET OSDKCPPFLAGS=-DLANG=2
 SET OSDKNAME=POG_OP1_ES
 GOTO End
 :Italian
-SET OSDKPREPROC=-DLANG=3
+SET OSDKCPPFLAGS=-DLANG=3
 SET OSDKNAME=POG_OP1_IT
 GOTO End
 
